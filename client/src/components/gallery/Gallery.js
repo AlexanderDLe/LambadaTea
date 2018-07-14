d1b3741c561c1d342afd78c55feb8f8f9f1d8389
Alexander Le <iamxanofficial@gmail.com>
2018-07-14 16:09:37 -0700

    Respond

--- client/src/components/gallery/Gallery.js
+++ client/src/components/gallery/Gallery.js
@@ -24,7 +24,7 @@ import Image21 from '../../img/Gallery/P21_result.webp';
 class Gallery extends Component {
   renderGalleryItem(img, name) {
     return (
-      <div className="col-lg-4 box">
+      <div className="col-lg-4 box mb-3">
         <div className="imgBox">
           <a
             href={img}
@@ -45,41 +45,29 @@ class Gallery extends Component {
     return (
       <section id="gallery" className="py-5">
         <div className="container">
-          <h1 className="text-center Title">Gallery</h1>
+          <h1 className="text-center Title">Gallery Test</h1>
           <hr />
           <br />
           <br />
-          <div className="row mb-4">
+          <div className="row">
             {this.renderGalleryItem(Image2, 'Enjoy our refreshing drinks!')}
             {this.renderGalleryItem(Image1, 'Welcome to Lambada!')}
             {this.renderGalleryItem(Image3, 'Fresh ingredients everyday!')}
-          </div>
-          <div className="row mb-4">
             {this.renderGalleryItem(Image4, 'Man-Go Crazy')}
             {this.renderGalleryItem(Image5, 'Peachberry Tea')}
             {this.renderGalleryItem(Image6, 'Mango Tango')}
-          </div>
-          <div className="row mb-4">
             {this.renderGalleryItem(Image7, 'Strawberry Wild')}
             {this.renderGalleryItem(Image8, "Pistachio Luv'n")}
             {this.renderGalleryItem(Image9, 'MMM-Mocha')}
-          </div>
-          <div className="row mb-4">
             {this.renderGalleryItem(Image10, 'Strawber-chee')}
             {this.renderGalleryItem(Image11, 'Taro Milk Tea')}
             {this.renderGalleryItem(Image12, 'Taro Temptation')}
-          </div>
-          <div className="row mb-4">
             {this.renderGalleryItem(Image13, 'Buttery Nut')}
             {this.renderGalleryItem(Image14, 'Avocado Nut')}
             {this.renderGalleryItem(Image15, 'Jasmine Milk Tea')}
-          </div>
-          <div className="row mb-4">
             {this.renderGalleryItem(Image16, 'Strawberry Suave')}
             {this.renderGalleryItem(Image17, 'Frizzled Wings')}
             {this.renderGalleryItem(Image18, 'Egg Rolls')}
-          </div>
-          <div className="row mb-4">
             {this.renderGalleryItem(Image19, 'Pho Satay Dac Biet (Dry)')}
             {this.renderGalleryItem(Image20, 'Seasoned Fries')}
             {this.renderGalleryItem(Image21, 'Pho Dac Biet')}
