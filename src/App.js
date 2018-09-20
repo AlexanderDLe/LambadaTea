3a6541926e3dba8989042d7c6705d2687a18fca7
AlexanderDLe <iamxanofficial@gmail.com>
2018-09-20 16:31:56 -0700

    Reformatted folder contents.  Removed Form/Backend

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import About from './components/about/About';
+import Footer from './components/layout/Footer';
+import Gallery from './components/gallery/Gallery';
+import Contact from './components/contact/Contact';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Landing />
+          <About />
+          <Gallery />
+          <Contact />
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div className="contact-form">
+      <h1 className="text-center">Contact</h1>
+    </div>
+  );
+};
